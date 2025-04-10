cbfdc17cec0d258006da190ace1f7c2e77a94b82
Parth090805 <parora1_be23@thapar.edu>
2025-04-10 17:33:19 +0530

    get URLsFromHTML function and tests

--- crawl.test.js
+++ crawl.test.js
@@ -1,4 +1,4 @@
-const {normalizeURL} = require('./crawl.js')
+const {normalizeURL, getURLsFromHTML} = require('./crawl.js')
 const {test, expect} = require('@jest/globals')
   
 test('normalizeURL strip protocol', () => {
@@ -29,3 +29,69 @@ test('normalizeURL strip http', () => {
   expect(actual).toEqual(expected)
 })
 
+test('getURLsFromHTML absolute', () => {
+  const inputHTMLBody = `
+<html>
+  <body>
+      <a href = "https://blog.boot.dev/path/">
+      BOOT.dev Blog
+      </a>
+  </body>
+</html>
+`
+const inputBaseUrl = "https://blog.boot.dev/path/"
+  const actual = getURLsFromHTML(inputHTMLBody, inputBaseUrl)
+  const expected = ["https://blog.boot.dev/path/"]
+  expect(actual).toEqual(expected)
+})
+
+test('getURLsFromHTML relative', () => {
+  const inputHTMLBody = `
+<html>
+  <body>
+      <a href = "/path/">
+      BOOT.dev Blog
+      </a>
+  </body>
+</html>
+`
+  const inputBaseUrl = "https://blog.boot.dev"
+  const actual = getURLsFromHTML(inputHTMLBody, inputBaseUrl)
+  const expected = ["https://blog.boot.dev/path/"]
+  expect(actual).toEqual(expected)
+})
+
+test('getURLsFromHTML both', () => {
+  const inputHTMLBody = `
+<html>
+  <body>
+      <a href = "https://blog.boot.dev/path1/">
+      BOOT.dev Blog path One
+      </a>
+      <a href = "/path2/">
+      Boot.dev Blog path Two
+      </a>
+  </body>
+</html>
+`
+  const inputBaseUrl = "https://blog.boot.dev"
+  const actual = getURLsFromHTML(inputHTMLBody, inputBaseUrl)
+  const expected = ["https://blog.boot.dev/path1/","https://blog.boot.dev/path2/"]
+  expect(actual).toEqual(expected)
+})
+
+test('getURLsFromHTML invalid', () => {
+  const inputHTMLBody = `
+<html>
+  <body>
+      <a href = "invalid">
+      invalid URL
+      </a>
+  </body>
+</html>
+`
+  const inputBaseUrl = "https://blog.boot.dev"
+  const actual = getURLsFromHTML(inputHTMLBody, inputBaseUrl)
+  const expected = []
+  expect(actual).toEqual(expected)
+})
